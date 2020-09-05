c69407daad4b93e1b16e86568f20494d4ad02398
sarah rose <xsarahrosemeow@gmail.com>
2020-09-04 23:45:00 -0400

    not done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+const welcome = document.querySelector("h2");
+welcome.addEventListener("mouseenter", () => {
+  welcome.style.color = "goldenrod";
+});
+
+const headerTwoLets = document.getElementById("letsGo");
+function showLetsGo() {
+  document.getElementById("letsGo").style.display = "visible";
+}
+function hideLetsGo() {
+  document.getElementById("letsGo").style.display = "none";
+}
+function showOrHide() {
+  if (document.getElementById("letsGo").style.display == "visible") {
+    hideLetsGo();
+  } else {
+    showLetsGo();
+  }
+}
+
+headerTwoLets.addEventListener("click", () => {
+    if(headerTwoLets.style.visibility === "visible"){
+        display: hidden {
+            else {
+                display: visible
+            }
+        }
+    }
+});
