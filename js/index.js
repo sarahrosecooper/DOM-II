92337a03e5a09546f3ba7aa19b7f7c10f83502ad
sarah rose <xsarahrosemeow@gmail.com>
2020-09-07 21:02:19 -0400

    up to 7

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// 1.) mouseover
+
 // make welcome to the fun bus goldenrod upon hover (mouseover)
 
 const welcome = document.querySelector("h2");
@@ -5,12 +7,14 @@ welcome.addEventListener("mouseover", () => {
   welcome.style.color = "goldenrod";
 });
 
-// change destination pink upon double click
+// make lets go disappear
 
 const letsGo = document.getElementById("letsGo");
 letsGo.addEventListener("mouseenter", () => {
   letsGo.style.visibility = "hidden";
 });
+
+// 2.) mouseleave
 
 // make reappear after 550 milliseconds
 letsGo.addEventListener("mouseleave", function () {
@@ -22,7 +26,52 @@ function visibleHeader() {
   letsGo.style.visibility = "visible";
 }
 
+// 4.) dblclick
+
+// change destination pink upon double click
+
 const pDestination = document.querySelector(".content-destination p");
 pDestination.addEventListener("dblclick", () => {
   pDestination.style.color = "pink";
+});
+
+// 5.) wheel
+
+// lets go image: change size upon zooming
+
+function zoom(event) {
+  event.preventDefault();
+
+  scale += event.deltaY * -0.01;
+
+  // restrict scale
+  scale = Math.min(Math.max(0.125, scale), 4);
+
+  // apply scale transform
+  wheelMove.style.transform = `scale(${scale})`;
+}
+
+// creating wheel function
+
+let scale = 1;
+const wheelMove = document.querySelector("#wheel");
+wheelMove.onwheel = zoom;
+
+// 6.) mouseover
+
+// show date when mouseover middle button
+
+const date = document.querySelectorAll(".btn");
+date.forEach((buttons) => {
+  buttons.addEventListener("mousedown", displayDate);
+});
+
+function displayDate() {
+  document.getElementById("emptySpace").innerHTML = Date();
+}
+
+// 7.)
+
+window.addEventListener("load", (event) => {
+  alert("Enjoy the ride!");
 });
